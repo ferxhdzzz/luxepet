c674b3961da2d421cf40c7ecb5fbdac19e111fbb
Josue <Fimel505011@gmail.com>
2025-03-20 15:07:32 -0600

    Category

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 import express from "express"
+import categoryRoutes from "./src/routes/category.js";
+
 
 const app = express();
 
 app.use(express.json())
+
+app.use("/api/category", categoryRoutes);
 
 export default app;
