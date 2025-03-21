a21e5f28468e1ed06e73c7aa9465eaf9979a7ead
Josue <fimel505011@gmail.com>
2025-03-21 09:18:41 -0600

    Enteoria ya ta todo

--- backend/src/config.js
+++ backend/src/config.js
@@ -5,7 +5,7 @@ dotenv.config //ejecutamos la libreria, para acceder al punto env
 export const config ={
 
     db:{
-        URI: process.env.DB_URI || "mongodb+srv://ferhdzzz:Fermi1998@clusterferh.q0jrn.mongodb.net/Luxepet?retryWrites=true&w=majority&appName=ClusterFerH"
+        URI: process.env.DB_URI || "mongodb+srv://ferhdzzz:Fermi1998@clusterferh.q0jrn.mongodb.net/luxepet2?retryWrites=true&w=majority&appName=ClusterFerH"
     },
     server:{
         port: process.env.PORT || 4000,
