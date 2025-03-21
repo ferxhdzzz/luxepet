3dc5ad0067a37733fdf33e0b3785cbbcebd42c07
Fernanda <20220031@ricaldone.edu.sv>
2025-03-20 19:59:25 -0600

    cruds

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 import express from "express"
+import emloyeesRoutes from "./src/routes/employees.js"
+import customersRoutes from "./src/routes/customers.js"
+import suppliersRoutes from "./src/routes/suppliers.js"
+
 
 const app = express();
 
 app.use(express.json())
+
+app.use("/api/employees",emloyeesRoutes)
+app.use("/api/customers",customersRoutes)
+app.use("/api/suppliers",suppliersRoutes)
+
 
 export default app;
