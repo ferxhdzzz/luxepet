8c76e13af4b5ff5b5b16909f60b3686887c1af59
Josue <Fimel505011@gmail.com>
2025-04-25 14:55:02 -0600

    Dios mío, esto me dio dolor de cabeza.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,10 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Nav from './components/nav'
 import Home from './pages/user/home'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'  // Changed from 'react-router'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from "react";
 import HeroNav from './components/heronav'
 import Productsycat from './pages/ProductosyCatt'
-
-
 import Configuration from './pages/user/configuration'
 import ProductPage from './pages/user/productPage'
 
@@ -17,13 +15,18 @@ function App() {
       <Router>
         <Nav />
         <Routes>
-          {/* Rutas */}
+          {/* Rutas existentes */}
           <Route path="/" element={<Home />} />
           <Route path="/configuration" element={<Configuration />} />
-          <Route path="/products" element={<Productsycat /> }  />
+          <Route path="/products" element={<Productsycat />} />
           <Route path="/gato" element={<HeroNav />} />
           <Route path="/prueba" element={<ProductPage />} />
-          {/* Demas rutas por agregar */}
+          
+          {/* Nuevas rutas dinámicas para productos */}
+          <Route path="/product/:productId" element={<ProductPage />} />
+          <Route path="/producto/:productId" element={<ProductPage />} />
+          <Route path="/mascota/:tipoMascota" element={<ProductPage />} />
+          <Route path="/popular/:productId" element={<ProductPage />} />
         </Routes>
       </Router>
     </>
